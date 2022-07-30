b10b8ddbee33f807c7e545e35a88ec5aab4ca767
lluckyboi <1598273095@qq.com>
2022-07-30 15:59:03 +0800

    增加了私人存储池和公共存储池两个表

--- LPan.sql
+++ LPan.sql
@@ -1,3 +1,19 @@
+create table private
+(
+    user_id     int                     not null,
+    file_name   varchar(32)             not null,
+    file_id     int                     not null,
+    deleted     datetime                null comment '删除的时间',
+    father_path varchar(32) default '/' not null
+);
+
+create table public_file
+(
+    file_name varchar(32) not null,
+    file_id   bigint auto_increment
+        primary key
+);
+
 create table user
 (
     user_name varchar(32) default 'LPanUser' not null,
