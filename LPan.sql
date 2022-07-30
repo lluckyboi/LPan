52f293de34bf2321d0c69bcc6cbb50d8116700a3
lluckyboi <1598273095@qq.com>
2022-07-30 19:13:35 +0800

    修改private表以支持秒传等

--- LPan.sql
+++ LPan.sql
@@ -1,17 +1,20 @@
 create table private
 (
-    user_id     int                     not null,
-    file_name   varchar(32)             not null,
-    file_id     int                     not null,
-    deleted     datetime                null comment '删除的时间',
-    father_path varchar(32) default '/' not null
+    user_id      int                     not null,
+    file_name    varchar(32)             not null,
+    file_id      int                     not null,
+    deleted      datetime                null comment '删除的时间',
+    father_path  varchar(32) default '/' not null,
+    real_deleted tinyint     default 0   not null comment '真正删除，无法找回 该记录会在下一次清理时被删除'
 );
 
 create table public_file
 (
-    file_name varchar(32) not null,
+    file_name varchar(32)  not null,
     file_id   bigint auto_increment
-        primary key
+        primary key,
+    hash      varchar(255) not null,
+    size      bigint       not null
 );
 
 create table user
@@ -19,6 +22,7 @@ create table user
     user_name varchar(32) default 'LPanUser' not null,
     user_id   bigint auto_increment
         primary key,
-    user_mail varchar(32)                    not null
+    user_mail varchar(32)                    not null,
+    vip       tinyint     default 0          not null
 );
 
