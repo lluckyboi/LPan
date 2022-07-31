8d717a0086f6931603c75c197ba8a690f032322e
lluckyboi <1598273095@qq.com>
2022-07-31 12:39:55 +0800

    修改架构图 新增大小限制

--- dao/cron.go
+++ dao/cron.go
@@ -8,7 +8,7 @@ import (
 func CleanDeletedFile() {
 	c := cron.New()
 	c.AddFunc("@daily", func() {
-		Db.Exec("delete from private where deleted<?", time.Now().Add(time.Hour*24*30))
+		Db.Exec("delete from private where deleted<=? or (share=1 and expr_time <= ?)", time.Now().Add(time.Hour*24*30), time.Now())
 	})
 	c.Start()
 	select {}
