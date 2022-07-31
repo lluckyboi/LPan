0d685c9ba1b016231bd3d8392aeaefc0cca4013f
lluckyboi <1598273095@qq.com>
2022-07-31 10:16:28 +0800

    新增url映射表 实现链接加密

--- LPan.sql
+++ LPan.sql
@@ -1,10 +1,12 @@
 create table private
 (
-    user_id      int                     not null,
-    file_name    varchar(32)             not null,
-    file_id      int                     not null,
-    deleted      datetime                null comment '删除的时间',
-    father_path  varchar(32) default '/' not null,
+    user_id     int                     not null,
+    file_name   varchar(32)             not null,
+    file_id     int                     not null,
+    deleted     datetime                null comment '删除的时间',
+    father_path varchar(32) default '/' not null,
+    share       tinyint     default 0   not null comment '表示这条记录是否为分享的',
+    expr_time   time                    null comment '分享文件过期时间'
 );
 
 create table public_file
@@ -14,6 +16,14 @@ create table public_file
         primary key,
     hash      varchar(255) not null,
     size      bigint       not null
+);
+
+create table url
+(
+    origin varchar(255) not null,
+    sha1   varchar(255) not null,
+    constraint url_sha1_uindex
+        unique (sha1)
 );
 
 create table user
