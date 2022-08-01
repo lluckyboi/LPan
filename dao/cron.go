dc2e5475f873c8f98d10765497a8d82a5c2397b7
lluckyboi <1598273095@qq.com>
2022-08-01 11:03:11 +0800

    优化错误处理

--- dao/cron.go
+++ dao/cron.go
@@ -2,13 +2,17 @@ package dao
 
 import (
 	"github.com/robfig/cron/v3"
+	"log"
 	"time"
 )
 
 func CleanDeletedFile() {
 	c := cron.New()
 	c.AddFunc("@daily", func() {
-		Db.Exec("delete from private where deleted<=? or (share=1 and expr_time <= ?)", time.Now().Add(time.Hour*24*30), time.Now())
+		_, err := Db.Exec("delete from private where deleted<=? or (share=1 and expr_time <= ?)", time.Now().Add(time.Hour*24*30), time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 	c.Start()
 	select {}
